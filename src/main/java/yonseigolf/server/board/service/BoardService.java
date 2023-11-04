7024ed2a573540a10e2762898c1453958e9b278d
birdie <flaehdan@gmail.com>
2023-11-04 17:51:48 +0900

    feat: 카테고리 필터 추가

--- src/main/java/yonseigolf/server/board/service/BoardService.java
+++ src/main/java/yonseigolf/server/board/service/BoardService.java
@@ -12,6 +12,7 @@ import yonseigolf.server.board.dto.response.BoardDetailResponse;
 import yonseigolf.server.board.dto.response.SingleBoardResponse;
 import yonseigolf.server.board.dto.response.SingleReplyResponse;
 import yonseigolf.server.board.entity.Board;
+import yonseigolf.server.board.entity.Category;
 import yonseigolf.server.board.entity.Reply;
 import yonseigolf.server.board.exception.BoardNotFoundException;
 import yonseigolf.server.board.repository.BoardRepository;
@@ -34,9 +35,9 @@ public class BoardService {
         this.replyRepository = replyRepository;
     }
 
-    public Page<SingleBoardResponse> findAllBoard(Pageable pageable) {
+    public Page<SingleBoardResponse> findAllBoard(Pageable pageable, Category category) {
 
-        return boardRepository.findAllBoardPaging(pageable);
+        return boardRepository.findAllBoardPaging(pageable, category);
     }
 
     public void postBoard(CreateBoardRequest createBoardRequest, long userId) {
