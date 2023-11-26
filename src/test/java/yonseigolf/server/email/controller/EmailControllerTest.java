c32de7da2c6276701dbfdc2d16aba5f2f5dba430
birdie <flaehdan@gmail.com>
2023-11-26 11:53:55 +0900

    test: email 리스트 조회 테스트

--- src/test/java/yonseigolf/server/email/controller/EmailControllerTest.java
+++ src/test/java/yonseigolf/server/email/controller/EmailControllerTest.java
@@ -5,11 +5,19 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.restdocs.payload.JsonFieldType;
+import yonseigolf.server.apply.entity.EmailAlarm;
 import yonseigolf.server.docs.utils.RestDocsSupport;
+import yonseigolf.server.email.dto.response.AllWaitingEmail;
 import yonseigolf.server.email.service.EmailService;
 
+import java.util.List;
+
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.doNothing;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -39,6 +47,41 @@ public class EmailControllerTest extends RestDocsSupport {
                 .andDo(document("email-apply-start-email",
                         getDocumentRequest(),
                         getDocumentResponse()));
+    }
+
+    @Test
+    @DisplayName("지원 대기자 목록을 조회할 수 있다.")
+    void applyWaitingList() throws Exception {
+        // given
+        AllWaitingEmail response = AllWaitingEmail.builder()
+                .emailAlarms(List.of(
+                        EmailAlarm.builder()
+                                .id(1L)
+                                .email("email@email.com")
+                                .build()
+                        )
+                )
+                .build();
+
+        given(emailService.findAllWaitingEmail()).willReturn(response);
+        // when
+
+        // then
+        mockMvc.perform(
+                get("/admin/email/apply-start-email")
+        ).andDo(print())
+                .andExpect(status().isOk())
+                .andDo(document("email-apply-waiting-list",
+                        getDocumentRequest(),
+                        getDocumentResponse(),
+                        responseFields(
+                                beneathPath("data").withSubsectionId("data"),
+                                fieldWithPath("emailAlarms").description("이메일 알림 목록"),
+                                fieldWithPath("emailAlarms[].id").type(JsonFieldType.NUMBER)
+                                        .description("이메일 알림 ID"),
+                                fieldWithPath("emailAlarms[].email").type(JsonFieldType.STRING)
+                                        .description("이메일")
+                        )));
     }
 
 
